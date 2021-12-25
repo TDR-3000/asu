9392c2fccb4ee15d9440cec146230aa25e591619
Angga YO <94930688+AnggaXD13@users.noreply.github.com>
2021-12-26 04:35:42 +0700

    Update opy.py

--- opy.py
+++ opy.py
@@ -1,4 +1,4 @@
-# Decompile by Lia
+# Decompile Angga Ady
 # with (uncompyle6) version : 3.7.4
 # Time Succes decompile : 2021-12-9 15:47:11.403269
 #Rekode Kontol
@@ -60,8 +60,8 @@ def clear():
     elif "win" in sys.platform.lower():os.system("cls")
     else:os.system("clear")
 def banner():
-    print("""  _______ _____   ___________________________ 
- \033[0;96m ___    |___  | / /__  ____/__  ____/___    |  ® \033[0m|| Recode By : Angga Ady
+    print(""" _______ _____   ___________________________ 
+ \033[0;96m.___    |___  | / /__  ____/__  ____/___    |  ® \033[0m|| Recode By : Angga Ady
  \033[0;96m __  /| |__   |/ / _  / __  _  / __  __  /| |      \033[0m|| Github.com/AnggaXD13
  \033[0;96m _  ___ |_  /|  /  / /_/ /  / /_/ /  _  ___ |  \033[0m|| Instagram: https://www.instagram.com/Anggatp55
  \033[0;96m /_/  |_|/_/ |_/   \____/   \____/   /_/  |_|  \033[0;91mv2.7  \033[0m|| Facebook.com/AnggaXD13
