f8bbab164aaf8ec0fcb0a4298632e16ec21504df
Angga YO <94930688+AnggaXD13@users.noreply.github.com>
2021-12-26 04:43:45 +0700

    Update opy.py

--- opy.py
+++ opy.py
@@ -216,7 +216,6 @@ def menu():
         ip = a["query"]
     except KeyError:
         ip = " "
-    print logo
     IP = requests.get('https://www.yayanxd.my.id/server/ip/').text
     print '___________________________________________________________\n';time.sleep(0.03)
     print ' (\033[0;96m•\033[0m) ACTIVE USER : %s'%(nama);time.sleep(0.03)
